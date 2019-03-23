13a4331f97c67126b9a062f45039b560ab3852e2
hassnaa <hassnaa_ayman@yahoo.com>
2019-03-24 00:55:30 +0200

    last update

--- simple server.js
+++ simple server.js
@@ -1,3 +1,5 @@
+
+///// simple server with express
 var express = require('express')
 const path = require('path')
 var app = express()
@@ -10,8 +12,18 @@ app.get('/myProfile', function (req, res) {
     res.send('myProfile')
 })
 
+
 app.get('/myImg/:id', function (req, res) {
     res.sendFile(path.join(__dirname, 'assets', req.params.id + '.jpg'))
 })
 
+
+/////////simple server without express
 app.listen(3000)
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead('200', { 'content-type': 'text/html' });
+    res.end('hello from simple node server');
+}).listen(5000);
